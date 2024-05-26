cef19afcdbcbbc096ed04c35d0baed51985d4949
Feofilaktov, Sergey <man@smairon.ru>
2024-05-26 17:00:07 +0300

    Replace mapping.Reference with DuplexMap structure

--- zodchy_patterns/mapping.py
+++ zodchy_patterns/mapping.py
@@ -1,21 +1,36 @@
+import typing
 import collections.abc
-import enum
+
+K = typing.TypeVar("K")
+V = typing.TypeVar("V")
 
 
-class Reference:
-    def __init__(self, data: collections.abc.Mapping[int, enum.Enum]):
+class DuplexMap(typing.Generic[K, V], typing.Mapping):
+    """Double way mapping"""
+
+    def __init__(self, data: collections.abc.Mapping[K, V]):
         self._data = data
 
-    def __getitem__(self, key: int) -> enum.Enum:
-        return self._data.get(key)
+    def __contains__(self, key: K) -> bool:
+        return key in self._data
 
-    def __len__(self) -> int:
+    def __getitem__(self, key: K) -> V:
+        return self._data[key]
+
+    def __iter__(self):
+        yield from self._data
+
+    def __len__(self) -> K:
         return len(self._data)
 
-    def get(self, key: int):
-        return self._data.get(key)
+    def get(
+        self,
+        key: K,
+        default: typing.Any | None = None
+    ) -> V | typing.Any:
+        return self._data.get(key, default)
 
-    def __call__(self, value: enum.Enum):
+    def __call__(self, value: V) -> K | None:
         for k, v in self._data.items():
             if v == value:
                 return k
